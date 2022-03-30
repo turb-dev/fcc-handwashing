324a4710b606d8438ca0fbd1dce1b2f56bdb1b91
Martin TS <71949714+turb-dev@users.noreply.github.com>
2022-03-30 15:06:10 +1300

    comitting html

---
+++ src/HowTo.svelte
@@ -0,0 +1,13 @@
+<div bp="grid">
+  <img
+    bp="offset-5@md 4@md 12@sm"
+    src="handwashing.gif"
+    alt="handwashing instructions"
+  />
+</div>
+
+<style>
+  img {
+    max-width: 100%;
+  }
+</style>
